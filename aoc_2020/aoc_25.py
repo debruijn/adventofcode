8ea0787a456e7ec2e82b4d63c8b817387e7bf614
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2023-01-17 15:28:11 +0100

    Committing input and examples for 2020 already to have them on another device that will be offline

---
+++ aoc_2020/aoc_25.py
@@ -0,0 +1,20 @@
+from typing import Union
+from util.util import ProcessInput, run_day
+
+debug = False
+
+
+def run_all(example_run: Union[int, bool]):
+
+    data = ProcessInput(example_run=example_run, day=25).as_int().data
+
+    result_part1 = "TODO"
+    result_part2 = "TODO"
+
+    extra_out = {'Number of rows in input': len(data)}  # TODO: create dict of additional things to have them printed
+
+    return result_part1, result_part2, extra_out
+
+
+if __name__ == "__main__":
+    run_day(run_all, [1])
