0c1614217d61026b5231b83926a0b4d5e196841d
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2023-03-04 19:56:13 +0100

    Formatting and updated unittest

--- tests/unittests/test_2020.py
+++ tests/unittests/test_2020.py
@@ -1,4 +1,4 @@
-from aoc_2020 import aoc_9, aoc_10, aoc_11, aoc_12, aoc_13
+from aoc_2020 import aoc_9, aoc_10, aoc_11, aoc_12, aoc_13, aoc_14
 import pytest
 
 import os
@@ -11,8 +11,9 @@ os.chdir('../../aoc_2020')
                           (aoc_11, [(37, 26)], (2386, 2091)),
                           (aoc_12, [(25, 286)], (998, 71586)),
                           (aoc_13, [(295, 1068781), (130, 3417), (295, 754018), (295, 779210), (295, 1261476),
-                                    (47, 1202161486)], (3215, 1001569619313439))],
-                         ids=["aoc_9", "aoc_10", "aoc_11", "aoc_12", "aoc_13"])
+                                    (47, 1202161486)], (3215, 1001569619313439)),
+                          (aoc_14, [(165, 'Not feasible'), (51, 208)], (15919415426101, 3443997590975))],
+                         ids=["aoc_9", "aoc_10", "aoc_11", "aoc_12", "aoc_13", "aoc_14"])
 def test_answers(day, expected_test, expected_actual):
     for test_nr in range(len(expected_test)):
         assert day.run_all(test_nr+1)[:2] == expected_test[test_nr]
