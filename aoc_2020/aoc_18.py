2aab1c741165683ebe028e66682bfb6b047567d7
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2023-10-09 12:10:11 -0400

    AoC 2020 day 18 incl part 2 and tests

--- aoc_2020/aoc_18.py
+++ aoc_2020/aoc_18.py
@@ -4,32 +4,56 @@ import itertools
 
 debug = False
 
-def calc_expression(expression):
 
+def calc_expression(expression, part2=False):
     if debug:
         print(f"Running for expression: {expression}")
 
     if expression.find('(') >= 0:
         first_i = expression.find('(')
         for i in itertools.count(start=first_i):
-            subexpr = expression[first_i:i+1]
+            subexpr = expression[first_i:i + 1]
             if subexpr.count('(') - subexpr.count(')') == 0:
-                subexpr_eval = calc_expression(subexpr[1:-1])
-                return calc_expression(expression[:first_i] + str(subexpr_eval) + expression[i+1:])
+                subexpr_eval = calc_expression(subexpr[1:-1], part2)
+                return calc_expression(expression[:first_i] + str(subexpr_eval) + expression[i + 1:], part2)
+    elif part2 and (expression.count('+') > 0):
+        split_expr = expression.split(' ')
+        for i, el in enumerate(split_expr):
+            if el == "+":
+                num = int(split_expr[i-1]) + int(split_expr[i+1])
+                new_expr = " ".join(split_expr[:i-1]) + f" {num} " + " ".join(split_expr[i+2:])
+                return calc_expression(new_expr.strip(" "), part2)
+
+        # Alternative: slower!
+        # # Find first plus -> split on it
+        # # Split first part on spaces to find final num
+        # # Split second part on spaces to find first num
+        # split_expr = expression.split('+', maxsplit=1)
+        # if split_expr[0].count("+") + split_expr[0].count("*") > 0:
+        #     before_expr, first_num, _ = split_expr[0].rsplit(" ", maxsplit=2)
+        # else:
+        #     first_num, _ = split_expr[0].split(" ")
+        #     before_expr = ""
+        # if split_expr[1].count("+") + split_expr[1].count("*") > 0:
+        #     _, second_num, after_expr = split_expr[1].split(" ", maxsplit=2)
+        # else:
+        #     _, second_num = split_expr[1].split(" ")
+        #     after_expr = ""
+        # full_expr = before_expr + f" {eval(first_num + ' + ' + second_num)} " + after_expr
+        # return calc_expression_add_first(full_expr.strip(" "))
     elif expression.count(' ') > 2:
         subexpr = " ".join(expression.split(" ")[:3])
         remainder = " ".join(expression.split(" ")[3:])
-        return calc_expression(str(eval(subexpr)) + ' ' + remainder)
+        return calc_expression(str(eval(subexpr)) + ' ' + remainder, part2)
     else:
         return eval(expression)
 
 
 def run_all(example_run: Union[int, bool]):
-
     data = ProcessInput(example_run=example_run, day=18).data
 
     result_part1 = sum(calc_expression(row) for row in data)
-    result_part2 = "TODO"
+    result_part2 = sum(calc_expression(row, part2=True) for row in data)
 
     extra_out = {'Number of rows in input': len(data)}  # TODO: create dict of additional things to have them printed
 
--- tests/unittests/test_2020.py
+++ tests/unittests/test_2020.py
@@ -1,4 +1,4 @@
-from aoc_2020 import aoc_9, aoc_10, aoc_11, aoc_12, aoc_13, aoc_14, aoc_15, aoc_16, aoc_17
+from aoc_2020 import aoc_9, aoc_10, aoc_11, aoc_12, aoc_13, aoc_14, aoc_15, aoc_16, aoc_17, aoc_18
 import pytest
 
 import os
@@ -15,9 +15,11 @@ os.chdir('../../aoc_2020')
                           (aoc_14, [(165, 'Not feasible'), (51, 208)], (15919415426101, 3443997590975)),
                           (aoc_15, [([436], [175594])], ([496], [883])),
                           (aoc_16, [(71, 1)], (27870, 3173135507987)),
-                          (aoc_17, [(112, 848)], (315, 1520))
+                          (aoc_17, [(112, 848)], (315, 1520)),
+                          (aoc_18, [(71, 231), (26335, 693891)], (4940631886147, 283582817678281))
                           ],
-                         ids=["aoc_9", "aoc_10", "aoc_11", "aoc_12", "aoc_13", "aoc_14", "aoc_15", "aoc_16", "aoc_17"])
+                         ids=["aoc_9", "aoc_10", "aoc_11", "aoc_12", "aoc_13", "aoc_14", "aoc_15", "aoc_16", "aoc_17",
+                              "aoc_18"])
 def test_answers(day, expected_test, expected_actual):
     for test_nr in range(len(expected_test)):
         assert day.run_all(test_nr+1)[:2] == expected_test[test_nr]
