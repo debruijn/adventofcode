5400787c1a302323f8b448853743b5737476a58d
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2023-10-16 15:08:34 -0400

    AoC 2020 day 25 incl unit test

--- aoc_2020/aoc_25.py
+++ aoc_2020/aoc_25.py
@@ -8,10 +8,25 @@ def run_all(example_run: Union[int, bool]):
 
     data = ProcessInput(example_run=example_run, day=25).as_int().data
 
-    result_part1 = "TODO"
-    result_part2 = "TODO"
+    val = 1
+    loop = 0
+    subject_nr = 7
+    div_value = 20201227
+    while val not in data:
+        val = (val * subject_nr) % div_value
+        loop += 1
 
-    extra_out = {'Number of rows in input': len(data)}  # TODO: create dict of additional things to have them printed
+    new_val = 1
+    for i in range(loop):
+        if val == data[0]:
+            new_val = (new_val * data[1]) % div_value
+        else:
+            new_val = (new_val * data[0]) % div_value
+
+    result_part1 = new_val
+    result_part2 = "Merry Christmas"
+
+    extra_out = {'Number of keys in input': len(data)}  # TODO: create dict of additional things to have them printed
 
     return result_part1, result_part2, extra_out
 
