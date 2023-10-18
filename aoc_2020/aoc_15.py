acdca12db6f0de814f2f6f7d8a4c7c9d1454e6b0
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2023-10-18 14:46:57 -0400

    some alternative implementations for day 15 of 2020

--- aoc_2020/aoc_15.py
+++ aoc_2020/aoc_15.py
@@ -5,7 +5,50 @@ from collections import defaultdict
 debug = False
 
 
+def func_game_opt(data, num_iter):
+
+    result = []
+    for row in data:
+
+        N_low = num_iter
+        low_num_map = [-1] * N_low
+        for i, num in enumerate(row):
+            low_num_map[num] = i+1
+        num_map = {}
+        num = row[-1]
+
+        if N_low < num_iter:
+            for i in range(len(row), num_iter):
+                if num < N_low:
+                    low_num_map[num], num = i, i - low_num_map[num] if low_num_map[num] >= 0 else 0
+                else:
+                    num_map[num], num = i, i - num_map.get(num, i)  # Use default argument of get in case num is not in num_map
+        else:
+            for i in range(len(row), num_iter):
+                low_num_map[num], num = i, i - low_num_map[num] if low_num_map[num] >= 0 else 0
+
+        result.append(num)
+
+    return result
+
+
 def func_game(data, num_iter):
+
+    result = []
+    for row in data:
+
+        num_map = {num: i+1 for i, num in enumerate(row)}
+        num = row[-1]
+
+        for i in range(len(row), num_iter):
+            num_map[num], num = i, i - num_map.get(num, i)  # Use default argument of get in case num is not in num_map
+
+        result.append(num)
+
+    return result
+
+
+def func_game_old(data, num_iter):
 
     result = []
     for row in data:
@@ -33,10 +76,11 @@ def run_all(example_run: Union[int, bool]):
 
     data = ProcessInput(example_run=example_run, day=15).as_list_of_ints(',').data
 
-    result_part1 = func_game(data, 2020)
-    result_part2 = func_game(data, 30000000)
+    result_part1 = func_game_opt(data, 2020)
+    result_part2 = func_game_opt(data, 30000000)
 
-    extra_out = {'Number of rows in input': len(data)}  # TODO: create dict of additional things to have them printed
+    extra_out = {'Number of rows in input': len(data),
+                 'Maximum number in input': max([max(row) for row in data])}  # TODO: create dict of additional things to have them printed
 
     return result_part1, result_part2, extra_out
 
