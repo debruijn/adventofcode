a4c6cc9515294ab717b72323e0cf07484cfe7657
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2024-07-26 10:44:28 -0400

    2017 day 4

---
+++ aoc_2017/aoc_4.py
@@ -0,0 +1,38 @@
+from itertools import combinations
+from typing import Union
+from util.util import ProcessInput, run_day
+from collections import Counter
+
+debug = False
+
+
+def run_all(example_run: Union[int, bool]):
+
+    data = ProcessInput(example_run=example_run, day=4, year=2017).data
+
+    result_part1 = sum(len(set(row.split())) == len(row.split()) for row in data)
+
+    # Initial setup of part 2
+    # sum_valid = 0
+    # for row in data:
+    #     this_row = [Counter(x) for x in row.split()]
+    #     unique = True
+    #     for i, j in combinations(this_row, 2):
+    #         if i == j:
+    #             unique = False
+    #     sum_valid += unique
+
+    sum_valid = 0
+    for row in data:
+        this_row = [Counter(x) for x in row.split()]  # Converting to Counter removes the order
+        sum_valid += all([i != j for i, j in combinations(this_row, 2)])
+
+    result_part2 = sum_valid
+
+    extra_out = {'Number of potential passphrases in input': len(data)}
+
+    return result_part1, result_part2, extra_out
+
+
+if __name__ == "__main__":
+    run_day(run_all, [1, 2, 3])  # 388 too high
