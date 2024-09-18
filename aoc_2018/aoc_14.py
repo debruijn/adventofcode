61984323a3be3bf47e786bcfbe4abb5c7e87cbd5
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2024-09-18 12:13:22 -0400

    2018 day 14 add Rust implementation for speed-up

--- aoc_2018/aoc_14.py
+++ aoc_2018/aoc_14.py
@@ -1,6 +1,10 @@
 from typing import Union
 from util.util import ProcessInput, run_day
+from aoc_rust import find_recipe
 
+use_rust = True  # Ignore Python settings and run in Rust in naive way. Timings: 2.8s (Rust) vs 19.4s (Python with acc)
+
+# Python settings and explanation below
 use_strings = False
 accelerate_loop = True
 k = 4  # For use in acceleration
@@ -18,39 +22,46 @@ def run_all(example_run: Union[int, bool]):
 
     data = ProcessInput(example_run=example_run, day=14, year=2018).as_int().data
 
-    nr_skip = str(data[0]) if use_strings else data[0]
-    nr_take = 10
-    recipe = "37" if use_strings else [3, 7]
-    locs = [0, 1]
-
-    if use_strings:  # Second implementation
-        while len(recipe) < int(nr_skip) + nr_take or nr_skip not in recipe:
-            new = str(int(recipe[locs[0]]) + int(recipe[locs[1]]))
-            recipe += new
-            locs = [(locs[0] + 1 + int(recipe[locs[0]])) % len(recipe),
-                    (locs[1] + 1 + int(recipe[locs[1]])) % len(recipe)]
-
-        result_part1 = recipe[int(nr_skip):int(nr_skip) + nr_take]
-        result_part2 = recipe.index(nr_skip)
+    if use_rust:
+        result_part1, result_part2 = find_recipe(data[0])
+        result_part1 = "".join(str(x) for x in result_part1)
+        extra_out = {'Number of digits to skip': data[0]}
     else:
-        if accelerate_loop:  # Third implementation
-            while (len(recipe) < nr_skip + nr_take or
-                   str(nr_skip) not in "".join(str(x) for x in recipe[-7 - int(len(recipe)/k):])):
-                for _ in range(1+int(len(recipe)/k)):
+
+
+        nr_skip = str(data[0]) if use_strings else data[0]
+        nr_take = 10
+        recipe = "37" if use_strings else [3, 7]
+        locs = [0, 1]
+
+        if use_strings:  # Second implementation
+            while len(recipe) < int(nr_skip) + nr_take or nr_skip not in recipe:
+                new = str(int(recipe[locs[0]]) + int(recipe[locs[1]]))
+                recipe += new
+                locs = [(locs[0] + 1 + int(recipe[locs[0]])) % len(recipe),
+                        (locs[1] + 1 + int(recipe[locs[1]])) % len(recipe)]
+
+            result_part1 = recipe[int(nr_skip):int(nr_skip) + nr_take]
+            result_part2 = recipe.index(nr_skip)
+        else:
+            if accelerate_loop:  # Third implementation
+                while (len(recipe) < nr_skip + nr_take or
+                       str(nr_skip) not in "".join(str(x) for x in recipe[-7 - int(len(recipe)/k):])):
+                    for _ in range(1+int(len(recipe)/k)):
+                        new = [int(x) for x in str(recipe[locs[0]] + recipe[locs[1]])]
+                        recipe.extend(new)
+                        locs = [(locs[0] + 1 + recipe[locs[0]]) % len(recipe), (locs[1] + 1 + recipe[locs[1]]) % len(recipe)]
+            else:  # First implementation
+                while len(recipe) < nr_skip + nr_take or str(nr_skip) not in "".join(str(x) for x in recipe[-7:]):
                     new = [int(x) for x in str(recipe[locs[0]] + recipe[locs[1]])]
                     recipe.extend(new)
                     locs = [(locs[0] + 1 + recipe[locs[0]]) % len(recipe), (locs[1] + 1 + recipe[locs[1]]) % len(recipe)]
-        else:  # First implementation
-            while len(recipe) < nr_skip + nr_take or str(nr_skip) not in "".join(str(x) for x in recipe[-7:]):
-                new = [int(x) for x in str(recipe[locs[0]] + recipe[locs[1]])]
-                recipe.extend(new)
-                locs = [(locs[0] + 1 + recipe[locs[0]]) % len(recipe), (locs[1] + 1 + recipe[locs[1]]) % len(recipe)]
 
-        result_part1 = "".join(str(x) for x in recipe[nr_skip:nr_skip+nr_take])
-        result_part2 = "".join(str(x) for x in recipe).index(str(nr_skip))
+            result_part1 = "".join(str(x) for x in recipe[nr_skip:nr_skip+nr_take])
+            result_part2 = "".join(str(x) for x in recipe).index(str(nr_skip))
 
-    extra_out = {'Number of digits to skip': nr_skip,
-                 'Final length of recipe': len(recipe)}
+        extra_out = {'Number of digits to skip': nr_skip,
+                     'Final length of recipe': len(recipe)}
 
     return result_part1, result_part2, extra_out
 
--- aoc_2018/aoc_rust_2018/src/lib.rs
+++ aoc_2018/aoc_rust_2018/src/lib.rs
@@ -1,6 +1,7 @@
 use pyo3::prelude::*;
 
 
+// 2018 day 5, utility function in Rust
 #[pyfunction]
 fn run_polymerization<'a>(input: Vec<u8>) -> usize {
     let mut v = Vec::new();
@@ -17,9 +18,75 @@ fn run_polymerization<'a>(input: Vec<u8>) -> usize {
     v.len()
 }
 
+// 2018 day 14, utility function in Rust
+#[pyfunction]
+fn find_recipe<'a>(input: usize) -> (Vec<usize>, usize) {
+
+    // part 1 in its own namespace
+    let part1 = {
+        let mut recipes = vec![3, 7];
+        let mut p1 = 0;
+        let mut p2 = 1;
+        let nr_take = 10;
+        while recipes.len() < input + nr_take {
+            let mut score = recipes[p1] + recipes[p2];
+
+            // Push to recipes for one digit at a time
+            if score >= 10 {
+                recipes.push(score / 10);
+                score %= 10;
+            }
+            recipes.push(score);
+
+            // New p
+            p1 = (p1 + 1 + recipes[p1]) % recipes.len();
+            p2 = (p2 + 1 + recipes[p2]) % recipes.len();
+        }
+        recipes[input..input+nr_take].to_vec()
+    };
+
+    // part 2 - can immediately return when answer found
+    let mut recipes = vec![3, 7];
+    let mut p1 = 0;
+    let mut p2 = 1;
+    // input as vector in this case
+    let input: Vec<usize> = input.to_string().chars().map(|d|
+        d.to_digit(10).unwrap() as usize).collect();
+    let ans;
+    loop {
+        let mut score = recipes[p1] + recipes[p2];
+        let mut two_digit = false;
+        if score >= 10 {
+            two_digit = true;
+            recipes.push(score / 10);
+            score %= 10;
+        }
+        recipes.push(score);
+        p1 = (p1 + 1 + recipes[p1]) % recipes.len();
+        p2 = (p2 + 1 + recipes[p2]) % recipes.len();
+
+        let recipe_len = recipes.len();
+        let input_len = input.len();
+        if recipe_len > input_len {  // can only check when recipe is long enough
+            if &recipes[recipe_len - input_len..recipe_len] == input {
+                ans = recipe_len - input_len;  // match final il digits
+                return (part1, ans);
+            }
+        }
+        if two_digit && recipe_len > input_len + 1 { // different ending possible for two_digit case
+            if &recipes[recipe_len - 1 - input_len..recipe_len - 1] == input {
+                ans = recipe_len - 1 - input_len;
+                return (part1, ans);
+            }
+        }
+    }
+}
+
+
 #[pymodule]
 #[pyo3(name="aoc_rust")]
 fn aoc_rust_2018(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(run_polymerization, m)?)?;
+    m.add_function(wrap_pyfunction!(find_recipe, m)?)?;
     Ok(())
 }
