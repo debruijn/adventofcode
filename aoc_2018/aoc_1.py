09fc275b363449b8a33073b6c9382318064d5758
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2024-09-19 13:52:13 -0400

    2018 day 1 part 2 add optional rust function

--- aoc_2018/aoc_1.py
+++ aoc_2018/aoc_1.py
@@ -1,5 +1,9 @@
 from typing import Union
 from util.util import ProcessInput, run_day
+from aoc_rust import get_frequency_shifts
+
+
+use_rust = False
 
 
 def run_all(example_run: Union[int, bool]):
@@ -7,7 +11,6 @@ def run_all(example_run: Union[int, bool]):
     data = ProcessInput(example_run=example_run, day=1, year=2018).data
     if len(data) > 1:
         data = [", ".join(data)]
-
     data = [int(x) for x in data[0].replace('+', '').split(', ')]
 
     # Part 1
@@ -16,27 +19,22 @@ def run_all(example_run: Union[int, bool]):
     # Part 2
     # This is the ugly but quick-to-write approach
     # Nicer would be a yield function that automatically reloops
-    stop = False
-    curr_freq = 0
-    curr_ind = 0
-    dict_of_freqs = {curr_freq}
-    nr_iter = 0
-    while not stop:
-        curr_freq += data[curr_ind]
-        if curr_ind + 1 < len(data):
-            curr_ind += 1
-        else:
-            curr_ind = 0
-            nr_iter += 1
-        if curr_freq in dict_of_freqs:
-            stop = True
-        else:
-            dict_of_freqs.add(curr_freq)
+    if not use_rust:
+        curr_freq, curr_ind = 0, 0
+        dict_of_freqs = {curr_freq}
+        while True:
+            curr_freq += data[curr_ind]
+            curr_ind = curr_ind + 1 if curr_ind < len(data) - 1 else 0
+            if curr_freq in dict_of_freqs:
+                break
+            else:
+                dict_of_freqs.add(curr_freq)
+    else:
+        curr_freq = get_frequency_shifts(data)
 
     result_part2 = curr_freq
 
-    extra_out = {'Number of frequency shifts to process': len(data),
-                 'Number of iterations through the process for part 2': nr_iter}
+    extra_out = {'Number of frequency shifts to process': len(data)}
 
     return result_part1, result_part2, extra_out
 
