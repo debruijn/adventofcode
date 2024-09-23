f66a82aaa2522fc64880ae6b49a0778a44bde02e
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2024-09-23 09:51:02 -0400

    Add comments to 2018 day 3 Rust function

--- aoc_2018/aoc_rust_2018/src/lib.rs
+++ aoc_2018/aoc_rust_2018/src/lib.rs
@@ -87,6 +87,7 @@ fn get_box_checksum_and_correct_id<'a>(input: Vec<String>) -> (isize, String) {
 // 2018 day 3
 #[pyfunction]
 fn process_contested_claims<'a>(input: Vec<[i16;5]>) -> (usize, i16) {
+    // For both parts: construct count of claims for each (r, c) coordinate
     let mut counts = HashMap::new();
     for claim in &input {
         for r in claim[1]..claim[1]+claim[3] {
@@ -96,6 +97,8 @@ fn process_contested_claims<'a>(input: Vec<[i16;5]>) -> (usize, i16) {
             }
         }
     }
+
+    // Part 1: count the coordinates with more than 1 claim
     let mut count_contested_claims = 0;
     for count in counts.values() {
         if *count > 1 {
@@ -103,6 +106,7 @@ fn process_contested_claims<'a>(input: Vec<[i16;5]>) -> (usize, i16) {
         }
     }
 
+    // Part 2: find the index of the claim without any (r, c) coordinates with a count larger than 1
     'for_claim: for claim in input {
         for r in claim[1]..claim[1]+claim[3] {
             for c in claim[2]..claim[2]+claim[4] {
@@ -113,7 +117,6 @@ fn process_contested_claims<'a>(input: Vec<[i16;5]>) -> (usize, i16) {
         }
         return (count_contested_claims, claim[0])
     }
-
     (count_contested_claims, 0)
 }
 
