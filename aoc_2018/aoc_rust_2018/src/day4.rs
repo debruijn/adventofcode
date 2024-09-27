eb3af52af64f3973e38b6f65c6f95dd3e11db162
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2024-09-27 18:33:39 -0400

    2018 day 4 in Rust

---
+++ aoc_2018/aoc_rust_2018/src/day4.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+use pyo3::pyfunction;
+use counter::Counter;
+use defaultmap::DefaultHashMap;
+
+
+fn most_sleepy_part_1(count_asleep: &HashMap<isize, Counter<isize>>) -> isize {
+    // Part 1
+    let most_sleepy_guard = count_asleep.keys().into_iter().max_by_key(
+        |x| count_asleep[x].total::<usize>()).unwrap();
+    let most_sleepy_minute = count_asleep[most_sleepy_guard].k_most_common_ordered(1)[0].0;
+    most_sleepy_guard * most_sleepy_minute
+}
+
+
+fn most_sleepy_part_2(count_asleep: &HashMap<isize, Counter<isize>>) -> isize {
+    // Part 1
+    let most_sleepy_guard = count_asleep.keys().into_iter().max_by_key(
+        |x| count_asleep[x].k_most_common_ordered(1)[0].1).unwrap();
+    let most_sleepy_minute = count_asleep[most_sleepy_guard].k_most_common_ordered(1)[0].0;
+
+    most_sleepy_guard * most_sleepy_minute
+}
+
+
+fn row_to_minute(row: &str) -> isize {
+    row.split("]").next().unwrap().
+        split(" ").last().unwrap().
+        split(":").last().unwrap().
+        parse().expect("Should be integer from input format")
+}
+
+
+// 2018 day 4 in Rust
+#[pyfunction]
+pub fn get_most_sleepy_guards(mut input: Vec<String>, sorted: bool) -> (isize, isize) {
+
+    if !sorted {
+        input.sort();
+    }
+
+    let mut count_asleep: DefaultHashMap<isize, Counter<isize>> = DefaultHashMap::default();
+    let mut curr_guard = -9999;
+    let mut start_sleep = 0;
+
+    for row in input.iter() {
+        if row.ends_with("begins shift") {
+            curr_guard = row[row.find("#").unwrap() + 1..]
+                .split(" ").next().unwrap()
+                .parse().expect("Should be integer from input format");
+        } else if row.ends_with("falls asleep") {
+            start_sleep = row_to_minute(row);
+        } else {
+            let end_sleep = row_to_minute(row);
+            let this_nap = (start_sleep..end_sleep).collect::<Counter<_>>();
+            count_asleep[curr_guard].extend(this_nap);
+        }
+    }
+
+    let count_asleep: HashMap<isize, Counter<isize>> = HashMap::from(count_asleep);
+    (most_sleepy_part_1(&count_asleep), most_sleepy_part_2(&count_asleep))
+}
--- aoc_2018/aoc_rust_2018/src/lib.rs
+++ aoc_2018/aoc_rust_2018/src/lib.rs
@@ -3,9 +3,9 @@ use pyo3::prelude::*;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 mod day5;
 mod day14;
-
 
 #[pymodule]
 #[pyo3(name="aoc_rust")]
@@ -16,7 +16,7 @@ fn aoc_rust_2018(m: &Bound<'_, PyModule>) -> PyResult<()> {
     // m.add_function(wrap_pyfunction!(day2::get_correct_box_ids, m)?)?;
     m.add_function(wrap_pyfunction!(day2::get_box_checksum_and_correct_id, m)?)?;
     m.add_function(wrap_pyfunction!(day3::process_contested_claims, m)?)?;
-
+    m.add_function(wrap_pyfunction!(day4::get_most_sleepy_guards, m)?)?;
     m.add_function(wrap_pyfunction!(day5::run_polymerization, m)?)?;
 
     m.add_function(wrap_pyfunction!(day14::find_recipe, m)?)?;
