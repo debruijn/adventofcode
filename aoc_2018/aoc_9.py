0c7c454d7b2473e76a0dc0ad801dea66086698fa
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2024-10-01 10:27:55 -0400

    2018 day 9 in Rust

--- aoc_2018/aoc_9.py
+++ aoc_2018/aoc_9.py
@@ -1,6 +1,9 @@
 from typing import Union
 from util.util import ProcessInput, run_day
 from collections import deque
+from aoc_rust import find_winning_score
+
+use_rust = True
 
 
 def run_all(example_run: Union[int, bool]):
@@ -8,40 +11,46 @@ def run_all(example_run: Union[int, bool]):
     data = ProcessInput(example_run=example_run, day=9, year=2018).as_list_of_ints().data
     nr_players, last_ball = data[0]
 
-    # Part 1: naive implementation
-    points = [0]*nr_players
-    circle = [0]
-    curr_ind = 0
-    curr_player = 0
+    if use_rust:
+        result_part1 = find_winning_score(nr_players, last_ball)
+        last_ball *= 100
+        result_part2 = find_winning_score(nr_players, last_ball)
 
-    for new_ball in range(1, last_ball + 1):
+    else:
+        # Part 1: naive implementation
+        points = [0]*nr_players
+        circle = [0]
+        curr_ind = 0
+        curr_player = 0
 
-        if new_ball % 23 == 0:
-            points[curr_player] += new_ball
-            curr_ind = (curr_ind - 7) % len(circle)
-            points[curr_player] += circle.pop(curr_ind)
-            curr_player = (curr_player + 1) % nr_players
-        else:
-            curr_ind = (curr_ind + 2) % len(circle)
-            circle.insert(curr_ind, new_ball)
-            curr_player = (curr_player + 1) % nr_players
+        for new_ball in range(1, last_ball + 1):
 
-    result_part1 = max(points)
+            if new_ball % 23 == 0:
+                points[curr_player] += new_ball
+                curr_ind = (curr_ind - 7) % len(circle)
+                points[curr_player] += circle.pop(curr_ind)
+                curr_player = (curr_player + 1) % nr_players
+            else:
+                curr_ind = (curr_ind + 2) % len(circle)
+                circle.insert(curr_ind, new_ball)
+                curr_player = (curr_player + 1) % nr_players
 
-    # Part 2: deque implementation - to stop inserting etc. Also: calculate current player from which ball we are at.
-    last_ball *= 100
-    points = [0]*nr_players
-    circle = deque([0])
+        result_part1 = max(points)
 
-    for new_ball in range(1, last_ball + 1):
-        if new_ball % 23 == 0:
-            circle.rotate(-7)
-            points[new_ball % nr_players] += new_ball + circle.pop()
-        else:
-            circle.rotate(2)
-            circle.append(new_ball)
+        # Part 2: deque implementation - to stop inserting etc. Also: calculate current player from which ball we are at.
+        last_ball *= 100
+        points = [0]*nr_players
+        circle = deque([0])
 
-    result_part2 = max(points)
+        for new_ball in range(1, last_ball + 1):
+            if new_ball % 23 == 0:
+                circle.rotate(-7)
+                points[new_ball % nr_players] += new_ball + circle.pop()
+            else:
+                circle.rotate(2)
+                circle.append(new_ball)
+
+        result_part2 = max(points)
 
     extra_out = {'Number of players in game': nr_players,
                  'Number of balls played (in game 1)': int(last_ball / 100)}
---
+++ aoc_2018/aoc_rust_2018/src/day9.rs
@@ -0,0 +1,23 @@
+use std::collections::VecDeque;
+use pyo3::pyfunction;
+
+// 2018 day 9, utility function in Rust
+#[pyfunction]
+pub fn find_winning_score<'a>(nr_players: usize, last_ball: usize) -> usize {
+    let mut circle = VecDeque::from([0]);
+    let mut points = vec![0; nr_players];
+
+    for new_ball in 1..=last_ball {
+        if new_ball % 23 == 0 {
+            circle.rotate_left(7);
+            points[new_ball % nr_players] += new_ball + circle.pop_back().unwrap();
+        } else {
+            if circle.len() >= 2 {
+                circle.rotate_right(2);
+            }
+            circle.push_back(new_ball);
+        }
+    }
+
+    *points.iter().max().unwrap()
+}
